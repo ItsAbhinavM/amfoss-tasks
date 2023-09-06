23abb8d2132e4a4ba1cfb5b8118d2af5a6edfa47
ItsAbhinavM <141326344+ItsAbhinavM@users.noreply.github.com>
2023-09-06 15:04:48 +0530

    Add files via upload

---
+++ task-03/prime-numbers.hs
@@ -0,0 +1,9 @@
+--printing prime numbers from a zero  --
+main :: IO ()
+main = do
+    n <- readLn
+    mapM_ print [i | i <- [2..n-1], isPrime i]
+    --The mapM_ function is used to apply the print function to each element in the list.--
+
+isPrime :: Int -> Bool
+isPrime i = all (\b -> i `mod` b /= 0) [2..i-1]
