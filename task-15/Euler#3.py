cc030b8c4136d1099913f6a6cf6c135dd40dbdaa
ItsAbhinavM <141326344+ItsAbhinavM@users.noreply.github.com>
2023-09-08 10:07:07 +0530

    Add files via upload

---
+++ task-15/Euler#3.py
@@ -0,0 +1,8 @@
+for i in range(int(input())):
+    a=int(input("Enter the number here : "))
+    cnt=[]
+    for i in range(2,a+1):
+        if a%i==0:
+            cnt.append(i)
+            a=a//2
+print(max(cnt))
