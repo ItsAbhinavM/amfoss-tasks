e4cc1ad3bcd5b24ea3320436a3067b886f2cb156
ItsAbhinavM <141326344+ItsAbhinavM@users.noreply.github.com>
2023-09-16 23:47:51 +0530

    Update Euler#5.py

--- task-15/Euler#5.py
+++ task-15/Euler#5.py
@@ -1,8 +1,8 @@
 import math
-t=int(input())
-for i in range(t):
-    n=int(input())
+var=int(input())
+for i in range(var):
+    n=int(input("Enter the range  : "))
     lst = []
     for item in range(1,n+1): 
         lst.append(item)
-    print(math.lcm(*lst))
+    print(math.lcm(*lst)) # prints the lcm of the numbers given in the list named lst
