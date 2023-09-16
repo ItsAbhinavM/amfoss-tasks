5f36ec2fece96a5f26ba32672692ed7570aca30f
ItsAbhinavM <141326344+ItsAbhinavM@users.noreply.github.com>
2023-09-16 22:09:06 +0530

    Update Euler#4.py

--- task-15/Euler#4.py
+++ task-15/Euler#4.py
@@ -1,19 +1,18 @@
 def is_palindrome(n):
     return str(n) == str(n)[::-1]
-
-l=[]
+the_list=[]
 
 for i in range(100,1000):
     for j in range(100,i+1):
         p=i*j
         if is_palindrome(p):
-            l.append(p)
-l.sort(reverse=True)
+            the_list.append(p)
+the_list.sort(reverse=True)
 
-t = int(input())
-for _ in range(t):
+inp = int(input())
+for _ in range(inp):
     n = int(input())
-    for i in l:
+    for i in the_list:
         if i<n:
             print(i)
             break
