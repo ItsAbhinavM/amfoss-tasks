b743df924261aeb8665d515c76260807b0ec3fce
ItsAbhinavM <141326344+ItsAbhinavM@users.noreply.github.com>
2023-09-22 13:03:31 +0530

    Update Euler#2.py

--- task-15/Euler#2.py
+++ task-15/Euler#2.py
@@ -5,7 +5,6 @@ for i in range(int(input())):
     while user_inp>=a:
         counter.append(a)
         a,b=b,a+b
-    print(set(counter))
     addition=0
     for i in counter:
         if i%2==0:
