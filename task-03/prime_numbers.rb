2ac4e661355dd51665139f04b3841d2832029971
ItsAbhinavM <141326344+ItsAbhinavM@users.noreply.github.com>
2023-09-30 21:42:35 +0530

    Update prime_numbers.rb

--- task-03/prime_numbers.rb
+++ task-03/prime_numbers.rb
@@ -1,8 +1,27 @@
-require 'prime'
+def is_prime(n)
+  return false if n <= 1
 
-puts "Enter a value: "
-limit = gets.chomp.to_i
+  (2..Math.sqrt(n)).each do |i|
+    return false if n % i == 0
+  end
 
-Prime.each(limit) do |prime|
-  puts prime
+  true
+end
+
+def find_primes_up_to(limit)
+  primes = []
+  (2..limit).each do |num|
+    primes << num if is_prime(num)
+  end
+  primes
+end
+
+puts "Enter a number: "
+user_input = gets.chomp.to_i
+
+if user_input < 2
+  puts "There are no prime numbers less than 2."
+else
+  prime_numbers = find_primes_up_to(user_input)
+  puts "Prime numbers from 2 to #{user_input}: #{prime_numbers.join(', ')}"
 end
