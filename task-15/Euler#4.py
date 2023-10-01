c2942ad365bb885d28bebed9f8635f440cc10ac6
ItsAbhinavM <141326344+ItsAbhinavM@users.noreply.github.com>
2023-10-01 11:16:54 +0530

    Update Euler#4.py

--- task-15/Euler#4.py
+++ task-15/Euler#4.py
@@ -1,18 +1,25 @@
 def is_palindrome(n):
     return str(n) == str(n)[::-1]
-the_list=[]
 
-for i in range(100,1000):
-    for j in range(100,i+1):
-        p=i*j
-        if is_palindrome(p):
-            the_list.append(p)
-the_list.sort(reverse=True)
+def find_palindromic_products(limit):
+    palindrome_products = []
+    for i in range(100, limit):
+        for j in range(100, i + 1):
+            product = i * j
+            if is_palindrome(product):
+                palindrome_products.append(product)
+    palindrome_products.sort(reverse=True)
+    return palindrome_products
 
-inp = int(input())
-for _ in range(inp):
-    n = int(input())
-    for i in the_list:
-        if i<n:
-            print(i)
-            break
+def main():
+    the_list = find_palindromic_products(1000)
+    
+    inp = int(input())
+    for _ in range(inp):
+        n = int(input())
+        for i in the_list:
+            if i < n:
+                print(i)
+                break
+
+main()
