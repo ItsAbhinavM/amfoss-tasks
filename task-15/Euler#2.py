11d382b335e3d061ec11208f5624c2e6eb3f1ef2
ItsAbhinavM <141326344+ItsAbhinavM@users.noreply.github.com>
2023-10-01 11:04:25 +0530

    Update Euler#2.py

--- task-15/Euler#2.py
+++ task-15/Euler#2.py
@@ -1,12 +1,21 @@
-for i in range(int(input())):
-    a,b=0,1
-    counter=[]
-    user_inp=int(input())
-    while user_inp>=a:
-        counter.append(a)
-        a,b=b,a+b
-    addition=0
-    for i in counter:
-        if i%2==0:
-            addition+=i
-    print(addition)
+def calculate_even_fibonacci_sum(limit):
+    a,b= 0, 1
+    lst= []
+    
+    while limit >= a:
+        lst.append(a)
+        a,b= b, a + b
+    
+    even_sum = 0
+    for num in lst:
+        if num % 2 == 0:
+            even_sum += num
+    
+    return even_sum
+
+n = int(input("Enter the number of test cases: "))
+
+for _ in range(n):
+    k = int(input("Enter a number: "))
+    result = calculate_even_fibonacci_sum(k)
+    print("Sum of even Fibonacci numbers up to", k, "is:", result)
